0f265d7d147f6c7069ca7ea3a27ab6734a9c7bc6
Noah Muldavin <nmuldavin@gmail.com>
2017-04-13 09:29:43 -0700

    verify method for passport local

--- src/server/auth/auth.js
+++ src/server/auth/auth.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const db = require('../db');
 
 /**
  * Security Options
@@ -21,7 +22,29 @@ const hashPass = (password, salt) => new Promise((resolve, reject) => {
   });
 });
 
+const checkPassword = (password, user, callback) => hashPass(password, user.salt)
+  .then((hashedPass) => {
+    if (hashedPass === user.hashedPass) {
+      return callback(null, user);
+    }
+
+    return callback(null, false);
+  });
+
+const verify = (username, password, callback) => {
+  db.get(username)
+    .then((user) => {
+      if (!user) {
+        return callback(null, false);
+      }
+
+      return checkPassword(password, user, callback);
+    })
+    .catch(err => callback(err));
+};
+
 module.exports = {
   generateSalt,
   hashPass,
+  verify,
 };
